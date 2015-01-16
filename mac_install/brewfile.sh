f2308a9f2a77ea72386ddaafcfe8c06a62078f92
Georgi Gardev <georgi@gardev.com>
2015-01-16 13:14:14 +0200

    Add initial version of mac installation scripts

---
+++ mac_install/brewfile.sh
@@ -0,0 +1,7 @@
+brew install git
+brew install tmux
+brew install zsh
+brew install macvim
+brew linkapps macvim
+
+brew install kdiff3
---
+++ mac_install/caskfile.sh
@@ -0,0 +1,28 @@
+# Install Cask
+brew install caskroom/cask/brew-cask
+
+# Install Casks
+brew cask install firefox
+brew cask install spotify
+brew cask install steam
+brew cask install adium
+brew cask install sublime-text
+brew cask install eclipse-java
+brew cask install evernote
+brew cask install skype
+
+brew cask install sizeup
+brew cask install cinch
+brew cask install battle-net
+brew cask install istat-menus
+brew cask install bartender
+brew cask install karabiner
+brew cask install macvim
+brew cask install google-chrome
+brew cask install the-unarchiver
+brew cask install magicprefs
+brew cask install dropbox
+brew cask install vlc
+
+# brew cask install divvy
+# brew cask install postbox
