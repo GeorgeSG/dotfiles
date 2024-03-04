8d55310e0139193f88d4cb2ff2e1c94be527a728
Georgi Gardev <georgi@gar.dev>
2024-03-04 22:25:26 +0200

    Update mac os brew setup

--- macos/brewfile.sh
+++ macos/brewfile.sh
@@ -2,7 +2,16 @@ brew install bash
 sudo chsh -s /usr/local/bin/bash
 
 brew install bash-git-prompt
+brew install bitwarden-cli
+brew install coreutils
+brew install docker
 brew install git
+brew install gh
+brew install htop
+brew install iperf3
+brew install jq
 brew install nvm
 brew install thefuck
 brew install tmux
+brew install tree
+brew install wget
