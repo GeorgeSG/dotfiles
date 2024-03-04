03a40e70a0971fb14bd4f8686a96c51f2e1c6fac
Georgi Gardev <georgi@gar.dev>
2024-03-04 23:50:08 +0200

    add install_brew.sh

---
+++ macos/install_brew.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Check for Homebrew and install if missing
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.bash_profile.local
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew update
+
+brew tap homebrew/bundle
+
+brew bundle --file ./macos/Brewfile
