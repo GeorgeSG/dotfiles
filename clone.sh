22c8a7159b32fdc1c8c0ac6f95374607708d9f44
Georgi Gardev <georgi@gar.dev>
2024-03-07 16:01:26 +0200

    Add clone file

---
+++ clone.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd ~/Git/dotfiles
+
+git clone git@github.com:GeorgeSG/advent-of-code.git
+git clone git@github.com:GeorgeSG/arc-cli.git
+git clone git@github.com:GeorgeSG/flame.git
+git clone git@github.com:GeorgeSG/gar.dev.git
+git clone git@github.com:GeorgeSG/ha-slack-user.git
+git clone git@github.com:ibkr-to-nra/ibkr-to-nra.git
+git clone git@github.com:GeorgeSG/isparkle-ble.git
+git clone git@github.com:GeorgeSG/lovelace-time-picker-card.git
+git clone git@github.com:GeorgeSG/maya-cv.git
+git clone git@github.com:GeorgeSG/PhotonaydenovV2.git
+git clone git@github.com:GeorgeSG/promptle.git
+git clone git@github.com:GeorgeSG/sarah.git
+git clone git@github.com:GeorgeSG/sub.rehab.git
+git clone git@github.com:GeorgeSG/taxbro.io.git
+
+git clone git@gitea:GeorgeSG/gardev-startpage.git
