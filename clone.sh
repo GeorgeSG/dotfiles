4a9239a1a052a3a28faa66a81c61181b4f467431
Georgi Gardev <georgi@gar.dev>
2024-03-09 15:45:53 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,19 +2,19 @@
 
 cd ~/Git/dotfiles
 
-git clone git@github.com:GeorgeSG/advent-of-code.git
-git clone git@github.com:GeorgeSG/arc-cli.git
-git clone git@github.com:GeorgeSG/flame.git
-git clone git@github.com:GeorgeSG/gar.dev.git
-git clone git@github.com:GeorgeSG/ha-slack-user.git
-git clone git@github.com:ibkr-to-nra/ibkr-to-nra.git
-git clone git@github.com:GeorgeSG/isparkle-ble.git
-git clone git@github.com:GeorgeSG/lovelace-time-picker-card.git
-git clone git@github.com:GeorgeSG/maya-cv.git
-git clone git@github.com:GeorgeSG/PhotonaydenovV2.git
-git clone git@github.com:GeorgeSG/promptle.git
-git clone git@github.com:GeorgeSG/sarah.git
-git clone git@github.com:GeorgeSG/sub.rehab.git
-git clone git@github.com:GeorgeSG/taxbro.io.git
+git clone git@github.com:GeorgeSG/advent-of-code.git ~/Git/advent-of-code
+git clone git@github.com:GeorgeSG/arc-cli.git ~/Git/arc-cli
+git clone git@github.com:GeorgeSG/flame.git ~/Git/flame
+git clone git@github.com:GeorgeSG/gar.dev.git ~/Git/gar.dev
+git clone git@github.com:GeorgeSG/ha-slack-user.git ~/Git/ha-slack-user
+git clone git@github.com:ibkr-to-nra/ibkr-to-nra.git ~/Git/ibkr-to-nra
+git clone git@github.com:GeorgeSG/isparkle-ble.git ~/Git/isparkle-ble
+git clone git@github.com:GeorgeSG/lovelace-time-picker-card.git ~/Git/lovelace-time-picker-card
+git clone git@github.com:GeorgeSG/maya-cv.git ~/Git/maya-cv
+git clone git@github.com:GeorgeSG/PhotonaydenovV2.git ~/Git/PhotonaydenovV2
+git clone git@github.com:GeorgeSG/promptle.git ~/Git/promptle
+git clone git@github.com:GeorgeSG/sarah.git ~/Git/sarah
+git clone git@github.com:GeorgeSG/sub.rehab.git ~/Git/sub.rehab
+git clone git@github.com:GeorgeSG/taxbro.io.git ~/Git/taxbro.io
 
-git clone git@gitea:GeorgeSG/gardev-startpage.git
+git clone git@gitea:GeorgeSG/gardev-startpage.git ~/Git/gardev-startpage
