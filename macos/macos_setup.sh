0810b5117b5bead5183da31f398214acf56cb5c9
Georgi Gardev <georgi@gar.dev>
2024-03-17 13:59:26 +0200

    mac os disable press and hold

--- macos/macos_setup.sh
+++ macos/macos_setup.sh
@@ -82,6 +82,9 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
