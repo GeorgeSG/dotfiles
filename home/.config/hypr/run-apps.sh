c775fb987e17a89dfeb80e768177abb294b1c78f
GeorgeSG <georgi@gardev.com>
2024-07-06 23:19:28 +0300

    Update eww and hypr

---
+++ home/.config/hypr/run-apps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+hyprctl dispatch -- exec '[workspace 1]' firefox &
+hyprctl dispatch -- exec '[workspace 3 silent]' telegram-desktop &
+sleep 1
+hyprctl dispatch -- exec '[workspace 3 silent]' todoist &
+sleep 1
+hyprctl dispatch -- exec '[workspace 3 silent]' alacritty -e btop &
